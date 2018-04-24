4b049daa43ca297ba33594d09e99d8136361f6ad
Unknown <timflorian1@gmail.com>
2018-04-24 16:41:03 +0300

    making async call ( using Future )
    timeSrt as a var to change dynamically

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,11 +7,14 @@ import javax.inject._
 import play.api._
 import play.api.mvc._
 
+import scala.concurrent.Future
+
 class Application @Inject() (components: ControllerComponents)
     extends AbstractController(components) {
-  def index = Action {
+  def index = Action.async {
     val date = new Date()
-    val dateStr = new SimpleDateFormat().format(date)
-    Ok(views.html.index(dateStr))
+    val dateStr = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(date)
+    Future.successful {Ok(views.html.index(dateStr))}
+
   }
 }
