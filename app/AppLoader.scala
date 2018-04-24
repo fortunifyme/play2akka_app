78cff0bfb11a38649db992563be5cc5feae5d974
Unknown <timflorian1@gmail.com>
2018-04-25 00:10:35 +0300

    add filter
    statsFilter

--- app/AppLoader.scala
+++ app/AppLoader.scala
@@ -1,13 +1,17 @@
 import com.softwaremill.macwire._
 import _root_.controllers.{Application, AssetsComponents}
+import filter.StatsFilter
 import play.api.ApplicationLoader.Context
 import play.api._
 import play.api.libs.ws.ahc.AhcWSComponents
 import play.api.mvc._
 import play.api.routing.Router
+import play.core.WebCommands
 import play.filters.HttpFiltersComponents
 import router.Routes
 import services.{SunService, WeatherService}
+
+import scala.concurrent.Future
 
 class AppApplicationLoader extends ApplicationLoader {
   def load(context: Context) = {
@@ -29,5 +33,20 @@ class AppComponents(context: Context) extends BuiltInComponentsFromContext(conte
   lazy val sunService =  wire[SunService]
   lazy val weatherService =  wire[WeatherService]
 
+  applicationLifecycle.addStopHook {()=>
+
+
+    Logger.info("The app is about to stop")
+    Future.successful(Unit)
+  }
+  /**
+    * if you wanna perform some task before application is started
+    */
+  val onStart = {
+    Logger.info ("The app is about to start")
+  }
+
+  lazy val statsFilter:Filter = wire[StatsFilter]
+  override lazy val httpFilters= Seq(statsFilter)
 
 }
---
+++ app/filter/StatsFilter.scala
@@ -0,0 +1,18 @@
+package filter
+
+import akka.actor.ActorSystem
+import akka.stream.Materializer
+import play.api.Logger
+import play.api.mvc.{Filter, RequestHeader, Result}
+
+import scala.concurrent.Future
+
+class StatsFilter(actorSystem: ActorSystem, implicit val mat: Materializer) extends Filter {
+
+  override def apply(nextFilter: (RequestHeader) => Future[Result])
+                    (header: RequestHeader): Future[Result] = {
+    Logger.info(s"Service another request : ${header.path}")
+    nextFilter(header)
+  }
+
+}
